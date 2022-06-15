47a376dc0c9e130f1fb7dd2f0229a90326eda4a7
Pablo C. Ruíz <pcruiher089@gmail.com>
2022-06-15 18:40:27 -0500

    new notebook

--- ffp.py
+++ ffp.py
@@ -332,69 +332,7 @@ class HyperHeuristic:
     print("=====================")
     exit(0)
 
-# A dummy hyper-heuristic for testing purposes.
-# The hyper-heuristic creates a set of randomly initialized rules.
-# Then, when called, it measures the distance between the current state and the
-# conditions in the rules
-# The rule with the condition closest to the problem state is the one that fires
-class DummyHyperHeuristic(HyperHeuristic):
-
-  # Constructor
-  #   features = A list with the names of the features to be used by this hyper-heuristic
-  #   heuristics = A list with the names of the heuristics to be used by this hyper-heuristic
-  #   nbRules = The number of rules to be contained in this hyper-heuristic
-  def __init__(self, features, heuristics, nbRules, seed):
-    super().__init__(features, heuristics)
-    random.seed(seed)
-    self.conditions = []
-    self.actions = []
-    for i in range(nbRules):
-      self.conditions.append([0] * len(features))
-      for j in range(len(features)):
-        self.conditions[i][j] = random.random()
-      self.actions.append(heuristics[random.randint(0, len(heuristics) - 1)])
-  
-  # Returns the next heuristic to use
-  #   problem = The FFP instance being solved
-  def nextHeuristic(self, problem):
-    minDistance = float("inf")
-    index = -1
-    state = []
-    for i in range(len(self.features)):
-      state.append(problem.getFeature(self.features[i]))
-    print("\t" + str(state))
-    for i in range(len(self.conditions)):
-      distance = self.__distance(self.conditions[i], state)      
-      if (distance < minDistance):
-        minDistance = distance
-        index = i
-    heuristic = self.actions[index] 
-    print("\t\t=> " + str(heuristic) + " (R" + str(index) + ")")
-    return heuristic
-
-  # Returns the string representation of this dummy hyper-heuristic
-  def __str__(self):
-    text = "Features:\n\t" + str(self.features) + "\nHeuristics:\n\t" + str(self.heuristics) + "\nRules:\n"
-    for i in range(len(self.conditions)):      
-      text += "\t" + str(self.conditions[i]) + " => " + self.actions[i] + "\n"      
-    return text
-
-  # Returns the Euclidian distance between two vectors
-  def __distance(self, vectorA, vectorB):
-    distance = 0
-    for i in range(len(vectorA)):
-      distance += (vectorA[i] - vectorB[i]) ** 2
-    distance = math.sqrt(distance)
-    return distance
-
-
-# A dummy hyper-heuristic for testing purposes.
-# The hyper-heuristic creates a set of randomly initialized rules.
-# Then, when called, it measures the distance between the current state and the
-# conditions in the rules
-# The rule with the condition closest to the problem state is the one that fires
 class PabloHH(HyperHeuristic):
-
   def __init__(self, features, heuristics, model):
     super().__init__(features, heuristics)
     self.model = model
@@ -403,7 +341,6 @@ class PabloHH(HyperHeuristic):
     featuresInStep = np.array(problem.retrieveFeaturesActualState())
     heuristic = self.model.predict([featuresInStep])
     print(featuresInStep)
-    
     print("\t\t=> " + str(heuristic) )
     return heuristic
 
@@ -415,46 +352,48 @@ class PabloHH(HyperHeuristic):
 # =====================
 
 isTraining = True
-directory = 'instances/onlytests'
+trainingDirectory = 'instances/onlytests'
+testingDirectory = 'instances/testing'
 # iterate over files in
-# that directory
+# that trainingDirectory
 
 
 trainingX = []
 trainingY = []
 
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    # checking if it is a file
-    if os.path.isfile(f):
-      print(f)
-      
-      # Solves the problem using heuristic LDEG and one firefighter
-      problem1 = FFP(f)
-      problem1.solve("LDEG", 1, False)
-      print("LDEG = " + str(problem1.whatIsResult()))
+if(isTraining):
+  for filename in os.listdir(trainingDirectory):
+      f = os.path.join(trainingDirectory, filename)
+      # checking if it is a file
+      if os.path.isfile(f):
+        print(f)
+        
+        # Solves the problem using heuristic LDEG and one firefighter
+        problem1 = FFP(f)
+        problem1.solve("LDEG", 1, False)
+        print("LDEG = " + str(problem1.whatIsResult()))
 
-      # Solves the problem using heuristic GDEG and one firefighter
-      problem2 = FFP(f)
-      problem2.solve("GDEG", 1, False)
-      print("GDEG = " + str(problem2.whatIsResult()))
+        # Solves the problem using heuristic GDEG and one firefighter
+        problem2 = FFP(f)
+        problem2.solve("GDEG", 1, False)
+        print("GDEG = " + str(problem2.whatIsResult()))
 
-      trainingXAux = []
-      trainingYAux = []
-      if(isTraining):
-        if(problem1.whatIsResult() <= problem2.whatIsResult()):
-          problem1.transformAndPrintTrainingX(0)
-          problem1.transformAndPrintTrainingY(0)
-          trainingXAux, trainingYAux = problem1.obtainTrainingData()
-        else:
-          problem2.transformAndPrintTrainingX(0)
-          problem2.transformAndPrintTrainingY(0)
-          trainingXAux, trainingYAux = problem2.obtainTrainingData()
+        trainingXAux = []
+        trainingYAux = []
+        if(isTraining):
+          if(problem1.whatIsResult() <= problem2.whatIsResult()):
+            problem1.transformAndPrintTrainingX(0)
+            problem1.transformAndPrintTrainingY(0)
+            trainingXAux, trainingYAux = problem1.obtainTrainingData()
+          else:
+            problem2.transformAndPrintTrainingX(0)
+            problem2.transformAndPrintTrainingY(0)
+            trainingXAux, trainingYAux = problem2.obtainTrainingData()
 
-        trainingX.extend(trainingXAux)
-        trainingY.extend(trainingYAux)
-        print(trainingX)
-        print(trainingY)
+          trainingX.extend(trainingXAux)
+          trainingY.extend(trainingYAux)
+          print(trainingX)
+          print(trainingY)
 
 supportVectorMachine = SVM()
 
@@ -466,16 +405,15 @@ if(isTraining):
   #dump into Pickle
   print("trained")
 else:
-  #test with HH
+  problem3 = FFP(f)
+  hh = PabloHH(["EDGE_DENSITY", "BURNING_NODES", "NODES_IN_DANGER", "BURNING_EDGES", "AVG_DEGREE"], ["LDEG", "GDEG"], supportVectorMachine)
+  print(hh)
+  problem3 = FFP(f)
+  problem3.solve(hh,1,False)
+
+  print("PabloHH = " + str(problem3.whatIsResult()))
   print()
 
 
-# Solves the problem using a randomly generated dummy hyper-heuristic
-problem3 = FFP(f)
-hh = PabloHH(["EDGE_DENSITY", "BURNING_NODES", "NODES_IN_DANGER", "BURNING_EDGES", "AVG_DEGREE"], ["LDEG", "GDEG"], supportVectorMachine)
-print(hh)
-problem3 = FFP(f)
-problem3.solve(hh,1,True)
 
-print("PabloHH = " + str(problem3.whatIsResult()))
 
